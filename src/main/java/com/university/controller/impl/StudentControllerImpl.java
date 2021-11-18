27b01aa09df0968cbb4952415dc66627aa9aa0f3
Ismayil <34961005+ismayilkarimli@users.noreply.github.com>
2021-11-18 18:58:49 +0400

    basic logic

--- src/main/java/com/university/controller/impl/StudentControllerImpl.java
+++ src/main/java/com/university/controller/impl/StudentControllerImpl.java
@@ -2,6 +2,7 @@ package com.university.controller.impl;
 
 import com.university.controller.StudentController;
 import com.university.dto.StudentDto;
+import com.university.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,27 +15,33 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class StudentControllerImpl implements StudentController {
 
+    private final StudentService studentService;
+
     @Override
     @PostMapping("")
-    public ResponseEntity<Map<String, Long>> createStudent(StudentDto dto) {
-        return null;
+    public ResponseEntity<Map<String, Long>> createStudent(@RequestBody StudentDto dto) {
+        var id = studentService.createStudent(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(id);
     }
 
     @Override
     @GetMapping("/{id}")
-    public ResponseEntity<StudentDto> getStudent(Long id) {
-        return null;
+    public ResponseEntity<StudentDto> getStudent(@PathVariable Long id) {
+        var student = studentService.getStudent(id);
+        return ResponseEntity.status(HttpStatus.OK).body(student);
     }
 
     @Override
     @PutMapping("/{id}")
-    public ResponseEntity<StudentDto> updateStudent(@PathVariable Long id, StudentDto dto) {
-        return null;
+    public ResponseEntity<StudentDto> updateStudent(@PathVariable Long id, @RequestBody StudentDto dto) {
+        var updatedStudent = studentService.updateStudent(id, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedStudent);
     }
 
     @Override
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteStudent(Long id) {
-        return null;
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
